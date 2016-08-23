fac63dbf78a7513139e73091a58e6fd1681fc07d
feimengwang <wjb27470@163.com>
2016-08-23 08:36:43 +0800

    refine Messenger

--- app/src/main/java/ipc/true123/cn/ipc/messenger/MessengerActivity.java
+++ app/src/main/java/ipc/true123/cn/ipc/messenger/MessengerActivity.java
@@ -5,12 +5,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
 import android.os.RemoteException;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -23,19 +25,28 @@ import ipc.true123.cn.ipc.R;
  * Created by junbo on 23/8/2016.
  */
 public class MessengerActivity extends AppCompatActivity {
+    public static final String TAG = "MessengerActivity";
     @BindView(R.id.sendmsg)
     Button sendMsg;
 
     @BindView(R.id.msg)
-    EditText msg;
+    EditText editText;
     Messenger messenger;
+    Messenger clientMessenger = new Messenger(new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Log.i(TAG, "msg from remote service is " + msg.getData().getString("msg"));
+            editText.setText(msg.getData().getString("msg"));
+            super.handleMessage(msg);
+        }
+    });
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.messenger_activity);
         ButterKnife.bind(this);
-        bindService(new Intent(this,MessengerService.class),connection, Context.BIND_AUTO_CREATE);
+        bindService(new Intent(this, MessengerService.class), connection, Context.BIND_AUTO_CREATE);
     }
 
     private ServiceConnection connection = new ServiceConnection() {
@@ -55,10 +66,12 @@ public class MessengerActivity extends AppCompatActivity {
     public void setSendMsg() {
         if (messenger != null) {
             Message message = Message.obtain(null, 1);
+            message.replyTo=clientMessenger;
             Bundle bundle = new Bundle();
-            bundle.putString("msg", msg.getText().toString());
+            bundle.putString("msg", editText.getText().toString());
             message.setData(bundle);
             try {
+
                 messenger.send(message);
             } catch (RemoteException e) {
                 e.printStackTrace();
--- app/src/main/java/ipc/true123/cn/ipc/messenger/MessengerService.java
+++ app/src/main/java/ipc/true123/cn/ipc/messenger/MessengerService.java
@@ -2,10 +2,12 @@ package ipc.true123.cn.ipc.messenger;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
@@ -20,7 +22,20 @@ public class MessengerService extends Service {
             switch (msg.what){
                 case 1:
                     Log.i(TAG,""+msg.getData().getString("msg"));
+                    if(msg.replyTo!=null){
+                        Messenger client = msg.replyTo;
+                        Message message = new Message();
+                        Bundle bundle = new Bundle();
+                        bundle.putString("msg","this is from remote service");
+                        message.setData(bundle);
+                        try {
+                            client.send(message);
+                        } catch (RemoteException e) {
+                            e.printStackTrace();
+                        }
+                    }
                     break;
+
             }
             super.handleMessage(msg);
         }
