2047777f26eaf12f7808cfa23b94a40eb450b568
feimengwang <wjb27470@163.com>
2016-08-23 08:00:01 +0800

    init

---
+++ app/src/main/java/ipc/true123/cn/ipc/messenger/MessengerService.java
@@ -0,0 +1,34 @@
+package ipc.true123.cn.ipc.messenger;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by junbo on 23/8/2016.
+ */
+public class MessengerService extends Service {
+    public static final String TAG="MessengerService";
+    Messenger messenger = new Messenger(new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case 1:
+                    Log.i(TAG,""+msg.getData());
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    });
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return messenger.getBinder();
+    }
+}
